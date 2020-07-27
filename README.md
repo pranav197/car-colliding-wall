9a8638a1626aa9c50635ab21f71da644632c610b
pranav197 <67427474+pranav197@users.noreply.github.com>
2020-07-27 03:29:01 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# car-colliding-wall
-car colliding wall
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+var wall, car;
+var speed,weight;
+
+function setup() {
+  
+  createCanvas(1600,400);
+ 
+  speed=random(55,90);
+  weight=random(400,1500);
+  car=createSprite(50, 200, 50, 20);
+  wall=createSprite(1500,200,60,height/2);
+ 
+}
+
+function draw() {
+  background(255,255,255);
+  car.velocityX = speed;
+  
+  drawSprites();
+}
